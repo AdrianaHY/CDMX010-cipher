6fb87ea9f83b5e1683268ef55fd22371ec913eff
Laboratoria <laboratoria@MacBook-Air-de-Laboratoria.local>
2020-11-25 16:03:37 -0600

    Add encode and decode functions, add styles and components

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,22 @@
 const cipher = {
-  // ...
+  encode: (offset, text) => {
+    let newWord = [];
+    let newOffset = parseInt(offset);
+    let letter;
+    for (let i = 0; i < text.length; i++) {
+      if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) {
+        letter = String.fromCharCode((text.charCodeAt(i) - 65 + newOffset) %26 + 65);
+        newWord.push(letter)
+      } else if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) {
+        letter = String.fromCharCode((text.charCodeAt(i) - 97 + newOffset) % 26 + 97);
+        newWord.push(letter)
+      }
+    }
+    return newWord.join('');
+  },
+  decode: (offset, text) => {
+    return cipher.encode(26 - (offset % 26), text);
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import cipher from './cipher.js';
 
+let firstScreen = document.getElementById('first-screen');
+let startButton = document.getElementById('start-button');
+let form = document.getElementById('form-container');
+let letsDecodeButton = document.getElementById('lets-decode-button');
+let decodeForm = document.getElementById('decode-form');
+
+let encodedButton = document.getElementById('encoded-button');
+let message = document.getElementById('message');
+let encoded = document.getElementById('encoded');
+let offset = document.getElementById('offset');
+
+let decodedButton = document.getElementById('decoded-button');
+let encodedMessage = document.getElementById('decoded-message');
+let decodedMessage = document.getElementById('decoded');
+let decodedOffset = document.getElementById('decoded-offset');
+
+encodedButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    encoded.innerHTML = cipher.encode(offset.value , message.value);
+});
+
+decodedButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    decodedMessage.innerHTML = cipher.decode(decodedOffset.value, encodedMessage.value);
+});
+
+startButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    firstScreen.style.display = 'none';
+    form.style.display = 'flex';
+});
+
+letsDecodeButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    decodeForm.style.display = 'flex';
+    form.style.display = 'none';
+});
+
 console.log(cipher);
